d1b0943836d111fc84c92b90a7b4e010daa4f5cd
carlosrobertoueda <carlos.ueda@gmail.com>
2023-03-05 05:54:37 -0300

    Projeto Inicial

---
+++ src/main/java/br/com/audsat/audsatseguros/AudsatSegurosApplication.java
@@ -0,0 +1,13 @@
+package br.com.audsat.audsatseguros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AudsatSegurosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AudsatSegurosApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/audsat/audsatseguros/AudsatSegurosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.audsat.audsatseguros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AudsatSegurosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
